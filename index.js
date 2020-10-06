9cca901f1c0a9775d86c7dd19645e2bd3b10c7c4
duclayan <duclayandominique@gmail.com>
2020-10-06 16:51:42 +0400

    changes

--- index.js
+++ index.js
@@ -17,8 +17,16 @@ class bankUser {
     };
 
     appendAccount(action,amount){
-        console.log(action)
-        this.balance = (action === 'deposit') ? this.balance+=amount : this.balance-=amount;
+      
+        if (action === 'deposit') {
+            this.balance+=amount
+        } else if (action === 'withdraw') {
+            if (this.balance>amount){
+                this.balance-=amount
+                } else if (this.balance<=amount){
+                    alert('Insufficient Balance')
+                }
+        }
         return this.balance;
     };
 
@@ -27,7 +35,6 @@ class bankUser {
         if (username !== '') {
             for (let i = 1; i < user.length; i++){
                 if (username === user[i].username){
-                    console.log('userFound')
                     return i
                 } 
             }
@@ -65,15 +72,12 @@ class bankUser {
 
         let i = this.checkExisting(recepient)
         var isEnoughBalance = (this.balance > amount) ? true : false;
-        console.log(isEnoughBalance)
 
         if (amount != NaN) {
             if (i !== false && isEnoughBalance === true) {
-                console.log("Success in transfer")
                 user[i].balance += amount
                 this.balance -= amount
             }else{
-                console.log("Fail to transfer")
                 document.getElementById('modal-status').style.display = 'block'
                 if  (i === false && isEnoughBalance ===false) {
                     alert("Insufficient Balance and Invalid User")
@@ -91,11 +95,17 @@ class bankUser {
     displayItems(stat){
         document.getElementById('input-container').style.display = `${stat}`;
         document.getElementById('modal-title').style.display = `${stat}`;
+        document.getElementById('modal-content-1').style.display = 'none';
+        document.getElementById('modal-content-2').style.display = 'none';
+        document.getElementById('modal-status').style.display = 'none';
+        document.getElementById("modal-amount").style.display = 'none';
+        modal.style.display = "none";
+        rcv.style.display = "none";
     }
 
-    addUser(image) {
+    addUser(imageLink) {
     let list = document.getElementById('output-list')
-    this.image = image
+    this.image = imageLink
     let newHTML = 
     `<li> 
     <img src = '${this.image}' height = "40px" width = "40px">
@@ -104,23 +114,23 @@ class bankUser {
     </li>`
     list.insertAdjacentHTML('beforeend',newHTML)
     }
-updateUserList() {
-    // list.innerHTML =''
-    for (let i = 1 ; i < user.length; i++){
-        console.log(user[i])
-        // let image = 'profile-picture.png'
-        let newHTML = 
-        `<li> 
-        <img src = '${user[i].image}' height = "40px" width = "40px">
-        <a> ${user[i].username} </a>
-        <a class = user-balance> ${user[i].balance} </a>
-        </li>`
-    
-        list.insertAdjacentHTML('beforeend',newHTML)
-        console.log('success')
+    updateUserList() {
+        // list.innerHTML =''
+        for (let i = 1 ; i < user.length; i++){
+            console.log(user[i])
+            // let image = 'profile-picture.png'
+            let newHTML = 
+            `<li> 
+            <img src = '${user[i].image}' height = "40px" width = "40px">
+            <a> ${user[i].username} </a>
+            <a class = user-balance> ${user[i].balance} </a>
+            </li>`
+        
+            list.insertAdjacentHTML('beforeend',newHTML)
+            console.log('success')
+        }
+        
     }
-    
-}
 }
 
 
@@ -138,59 +148,62 @@ user[5] = new bankUser ('Analyn', 250, 5, '1111', 'profile-picture.png')
 list = document.getElementById('output-list')
 // Add all the users to the list 
 currentUser = user[1]
+// call updateUserList to input existing users
 currentUser.updateUserList()
-// When the user clicks the button, open the modal 
+
 
 document.onclick = function(event) {
     current= event.target  
-    var action = ""
     var stat = true
     var showpage = ""
     let moneyDisplay = document.getElementById('current-money')
 
-    action = (current.id === '') ? current.parentElement.id : current.id 
+    var action = (current.id === '') ? current.parentElement.id : current.id 
     if (current.id === 'modal-title') {action = 'submit'}
-    console.log(action)
     document.getElementById('first-page').style.display = 'none'
 
     switch (action) {
         case 'sign-in-button':
             showpage= 'sign-in-page'
             break;
+
         case 'crt-acc-button':
             showpage= 'sign-up-page'
             break;
+
         case 'user-sign-in':
-            // user input the username and password
+
+            // get user input: username, password
             var username = document.getElementById('username').value;
             var pass1 = document.getElementById('pass').value;
-            // system checks whether username is existing
+
+            //verification check for user
             let userCheck = currentUser.checkExisting(username)
 
             // check if valid account
             if (userCheck !== false && user[userCheck].password === pass1) {
-                console.log("successful")
                 currentUser = user[userCheck]
-                
+                moneyDisplay.innerHTML = currentUser.balance
                 showpage = 'close'
-                document.getElementById('header-image').src =currentUser.image
+                document.getElementById('header-image').src = currentUser.image
+
             } else {
                 showpage = 'stay'
                 alert("Invalid Account")
             }
+            
             break;
+
         case 'user-sign-up':
             // INPUT username, password, and password verification
             var username = document.getElementById('sign-up-username').value;
             var pass1 = document.getElementById('password').value;
             var pass2 = document.getElementById('conf-password').value;
-            // var image = document.getAnimations('image').value;
-            // console.log(`this is the image ${image}`)
-            // INSTANTIATE newUser
+
             console.log(`Username: ${username}, Pass1: ${pass1}, Pass2: ${pass2}`)
             id = user.length
             user.push(username)
-            user[id] = new bankUser(username, 0,id);
+            user[id] = new bankUser(username, 0, id);
 
             
             // VALIDATION CHECK: Password Match & Existinguser
@@ -210,16 +223,7 @@ document.onclick = function(event) {
             };
             //ADDING IMAGE
             let list = document.getElementById('output-list')
-            // currentUser.image = `data:image/png;base64,`
-            currentUser.image = imageLink
-            let newHTML = 
-            `<li> 
-            <img src = '${currentUser.image}' height = "40px" width = "40px">
-            <a> ${currentUser.username} </a>
-            <a class = user-balance> ${currentUser.balance} </a>
-            </li>`
-            list.insertAdjacentHTML('beforeend',newHTML)
-            
+            currentUser.addUser(imageLink)
             console.log(currentUser.image)
             document.getElementById('header-image').src =currentUser.image
             document.getElementById('current-money').innerHTML = currentUser.balance
@@ -234,21 +238,20 @@ document.onclick = function(event) {
             break;
         case 'get-balance':
             currentUser.displayItems('none')
-            document.getElementById("modal-amount").style.display = 'block';
-            document.getElementById("modal-amount").innerHTML = currentUser.balance
-
+            let balanceDisplay = document.getElementById("modal-amount")
+            balanceDisplay.style.display = 'block';
+            balanceDisplay.innerHTML = currentUser.balance
             break;
         case 'submit':
             let inputAmount = parseFloat(document.getElementById('amount').value);
             let recepient = document.getElementById('receiver').value;
-
             if (previousAction === 'withdraw' || previousAction === 'deposit'){
                 currentUser.appendAccount(previousAction,inputAmount);
             } else if (previousAction === 'send-money'){
                 currentUser.transferMoney(inputAmount,recepient)
             }
-            action = 'close-window'
             moneyDisplay.innerHTML = currentUser.balance +'.00'
+            action = 'close-window'
             break;
         case 'user-list':
             showpage = 'user-list'
@@ -261,19 +264,16 @@ document.onclick = function(event) {
             stat = false
             break;
     }
+
     console.log(`Showpage: ${showpage}`)
     if (showpage === 'user-list') {
-        console.log('User List Should Appear')
         modal.style.display = 'block'
         document.getElementById('modal-content-2').style.display = 'flex';
     } else if (showpage !== 'close' && showpage !== '' && showpage !== 'stay'){ 
-        // display sign-in and sign-up modal
         document.getElementById(`${showpage}`).style.display = 'block';
     } else if(showpage == 'close') {
-        // closes sign-in and sign-up modal
         document.getElementById('signIn').style.display = 'none';
     } else  if (stat === true && showpage === '') {
-        console.log('regular display')
         document.getElementById('modal-title').innerHTML = action
         modal.style.display = 'block'
         document.getElementById('modal-content-1').style.display = 'flex';
@@ -281,23 +281,17 @@ document.onclick = function(event) {
         stat = false;
     }
     
-    
+// hold the action required
     if (action === "deposit" || action === 'withdraw' || action === 'send-money'){
         previousAction = action;
     }  else {
         console.log(`Saved Previous Action: ${previousAction}`)
     }
 
+// close modal window
     if (event.target.className === 'close'|| action === 'close-window') {
-        console.log("close")
-        modal.style.display = "none";
-        rcv.style.display = "none";
         currentUser.displayItems('block')
         input.value = null
-        document.getElementById('modal-content-1').style.display = 'none';
-        document.getElementById('modal-content-2').style.display = 'none';
-        document.getElementById('modal-status').style.display = 'none';
-        document.getElementById("modal-amount").style.display = 'none';
     }
 }
 
